b0d6aa8f59c473d90cd9d320f98a720d73fbd7f3
Heijden,Rafaël R.A.M. van der <rafael.vanderheijden@student.fontys.nl>
2024-01-10 00:24:56 +0100

    added navbar margin for content

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,8 +5,14 @@
     <div>
         <SnowParticles />
         <Navbar />
-        <div>
+        <div class="content">
             <slot />
         </div>
     </div>
 </template>
+
+<style scoped>
+.content {
+    margin-top: 80px;
+}
+</style>
